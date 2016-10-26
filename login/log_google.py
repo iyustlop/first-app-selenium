8c85686ab5c1114cf623e51cd53fb36a02c2eabb
iyustlop <iyustlop@gmail.com>
2016-10-26 22:00:36 +0200

    Firefox setup and changes in log_google to improve password insert

--- login/log_google.py
+++ login/log_google.py
@@ -1,4 +1,5 @@
 # Google login module
+from selenium.webdriver.support.ui import WebDriverWait
 
 def loggin_google (driver,usuario,password):
 	user=driver.find_element_by_id('Email')
@@ -8,10 +9,11 @@ def loggin_google (driver,usuario,password):
 	next=driver.find_element_by_id('next')
 	next.click()
 
-	driver.implicitly_wait(20) #//gives an implicit wait for 20 seconds
-
+	#driver.implicitly_wait(20) #//gives an implicit wait for 20 seconds
 	contrasena=driver.find_element_by_id('Passwd')
-	#contrasena.clear()
+	
+	wait = WebDriverWait(driver, 20)
+	contrasena.clear()
 	contrasena.send_keys(password)
 
 	next=driver.find_element_by_id('signIn')
