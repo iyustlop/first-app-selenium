dfd585174b4df1506f5c0ff6c4f7313bdb70b1a4
iyustlop <iyustlop@gmail.com>
2016-11-18 15:18:30 +0100

    Avoid find Passwrs before it is charge.

--- login/log_google.py
+++ login/log_google.py
@@ -9,11 +9,9 @@ def loggin_google (driver,usuario,password):
 	next=driver.find_element_by_id('next')
 	next.click()
 
-	#driver.implicitly_wait(20) #//gives an implicit wait for 20 seconds
+	#wait = WebDriverWait(driver, 20)
+	driver.implicitly_wait(20) #//gives an implicit wait for 20 seconds
 	contrasena=driver.find_element_by_id('Passwd')
-	
-	wait = WebDriverWait(driver, 20)
-	contrasena.clear()
 	contrasena.send_keys(password)
 
 	next=driver.find_element_by_id('signIn')
