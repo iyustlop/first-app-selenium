dcc9eb661f95e3657b125f90d612834f2a5d44bf
iyustlop <iyustlop@gmail.com>
2016-11-18 16:05:17 +0100

    Modificaion to avoid find Password before it is loaded

--- login/log_google.py
+++ login/log_google.py
@@ -9,11 +9,11 @@ def loggin_google (driver,usuario,password):
 	next=driver.find_element_by_id('next')
 	next.click()
 
+	wait = WebDriverWait(driver, 20)
 	#driver.implicitly_wait(20) #//gives an implicit wait for 20 seconds
 	contrasena=driver.find_element_by_id('Passwd')
-	
-	wait = WebDriverWait(driver, 20)
-	contrasena.clear()
+
+	#contrasena.clear()
 	contrasena.send_keys(password)
 
 	next=driver.find_element_by_id('signIn')
