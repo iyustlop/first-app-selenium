98e110c314ebfd5b018b88042e8ee292c5d3ba6c
iyustlop <iyustlop@gmail.com>
2017-06-28 23:45:12 +0200

    added initialization

--- user/user.py
+++ user/user.py
@@ -1,21 +1,19 @@
 import os
-from configparser import ConfigParser,RawConfigParser
+from configparser import ConfigParser
 
 class myUser:
 
+    def __init__(self):
+        self.parser = ConfigParser()
+        self.parser.read('/home/sobremesa/Documentos/first-app-selenium/user/example.cfg')
+
     def getUser(self):
-        parser = ConfigParser()
-        parser.read('/home/sobremesa/Documentos/first-app-selenium/user/example.cfg')
-        return parser.get('DEFAULT', 'email')
+        return self.parser.get('DEFAULT', 'email')
 
     def getPassword(self):
-        parser = ConfigParser()
-        parser.read('/home/sobremesa/Documentos/first-app-selenium/user/example.cfg')
-        return parser.get('DEFAULT', 'psw')
+        return self.parser.get('DEFAULT', 'psw')
 
     def getDriver(self):
-        parser = ConfigParser()
-        parser.read('/home/sobremesa/Documentos/first-app-selenium/user/example.cfg')
-        return parser.get('DEFAULT', 'driver')
+        return self.parser.get('DEFAULT', 'driver')
 
 
